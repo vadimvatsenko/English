0a0cb88fcbd4927927dfb8ad3c155379b3a408b5
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-03-21 20:49:54 +0200

    save 9

--- obj/Debug/net9.0/English.AssemblyInfo.cs
+++ obj/Debug/net9.0/English.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("English")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282d0c811c3b89a6739b63a177d6506cfef7e440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6565bdc4b5fabe95029b63d92e9a718d5cab2b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("English")]
 [assembly: System.Reflection.AssemblyTitleAttribute("English")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
