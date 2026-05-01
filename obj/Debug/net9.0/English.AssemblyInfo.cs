03c3ae338738793b255e969f516c87c82f79c45a
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-05-01 20:01:49 +0300

    add 31-32 A0

--- obj/Debug/net9.0/English.AssemblyInfo.cs
+++ obj/Debug/net9.0/English.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("English")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abaf96f27ae95b75547fc9dba5ccb507c3db1948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08543519a21feb9b69f6c212decc17dd3b965d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("English")]
 [assembly: System.Reflection.AssemblyTitleAttribute("English")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
