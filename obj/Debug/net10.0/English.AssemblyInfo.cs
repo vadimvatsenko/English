5e888c3dcd104171d57a2eeb6a8085a39cb695a2
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-07-27 10:31:45 +0300

    add new lesson

--- obj/Debug/net10.0/English.AssemblyInfo.cs
+++ obj/Debug/net10.0/English.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("English")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa49c4098d65845145547396f74d84d64a4468d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1864e2ab7413c145eb526350618b7071d69a9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("English")]
 [assembly: System.Reflection.AssemblyTitleAttribute("English")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
