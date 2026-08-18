ee49ee5730d221fa223404e5a11e0cec892a3a79
Vadym Vatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2026-08-18 09:34:41 +0300

    undate my progress

--- obj/Debug/net10.0/English.AssemblyInfo.cs
+++ obj/Debug/net10.0/English.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("English")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9c59707b9e44b98a5a7fa2e486e19d68a0d013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91488778b7103ff9399ca2c6dc3cb80d26691e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("English")]
 [assembly: System.Reflection.AssemblyTitleAttribute("English")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
