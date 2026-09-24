ef74d3ffce941d0d73e17361836909a18d853de6
Vadym Vatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2026-09-24 09:39:54 +0300

    add some answers and refatiring expressions

--- obj/Debug/net10.0/English.AssemblyInfo.cs
+++ obj/Debug/net10.0/English.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("English")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b4d67e0f4730e26cd5c786357f267f6a407bd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff811869875e26e3db93b1824b94b080b05462a")]
 [assembly: System.Reflection.AssemblyProductAttribute("English")]
 [assembly: System.Reflection.AssemblyTitleAttribute("English")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
